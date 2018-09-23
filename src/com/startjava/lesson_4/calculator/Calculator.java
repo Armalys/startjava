e02d94c391c0f4b22439e81a9343fbe45252e418
Armalys <mashnert@mail.ru>
2018-09-23 18:27:27 +0300

    first commit

--- src/com/startjava/lesson_4/calculator/Calculator.java
+++ src/com/startjava/lesson_4/calculator/Calculator.java
@@ -2,25 +2,23 @@ package com.startjava.lesson_4.calculator;
 
 public class Calculator {	
 	private String[] mathExpression;
-    private int firstNumber;
-    private int secondNumber;
 
-    public  Calculator (String[] mathExpression) {
+	public  Calculator (String[] mathExpression) {
         this.mathExpression = mathExpression;
     }
 
 	public void calculate() {
-	    firstNumber = Integer.parseInt(mathExpression[0]);
-	    secondNumber = Integer.parseInt(mathExpression[2]);
+		int firstNumber = Integer.parseInt(mathExpression[0]);
+		int secondNumber = Integer.parseInt(mathExpression[2]);
 		switch (mathExpression[1]) {
 			case "+":
 				System.out.println(firstNumber + secondNumber);
 				break;
 			case "-":
-				System.out.println(Math.subtractExact(firstNumber,secondNumber));
+				System.out.println(Math.subtractExact(firstNumber, secondNumber));
 				break;
 			case "*":
-				System.out.println(Math.multiplyExact(firstNumber,secondNumber));
+				System.out.println(Math.multiplyExact(firstNumber, secondNumber));
 				break;
 			case "/":
 				System.out.println(firstNumber / secondNumber);
@@ -29,7 +27,8 @@ public class Calculator {
 				System.out.println(firstNumber % secondNumber);
 				break;
 			case "^":
-				System.out.println(Math.pow(firstNumber,secondNumber));
+				System.out.println(Math.pow(firstNumber, secondNumber));
+				break;
 		}
 	}
 }
