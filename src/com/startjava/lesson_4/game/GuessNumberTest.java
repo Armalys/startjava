457d7bb6ecc9af16584eb8c52482b035c119824f
Armalys <mashnert@mail.ru>
2018-09-29 00:32:46 +0300

    code formatting

--- src/com/startjava/lesson_4/game/GuessNumberTest.java
+++ src/com/startjava/lesson_4/game/GuessNumberTest.java
@@ -5,33 +5,32 @@ import java.util.Scanner;
 public class GuessNumberTest {
 
     private static Scanner scanner = new Scanner(System.in);
-    private static String[] playersNames = new String[2];
-    private static String answer;
 
 	public static void main(String[] args) {
-        entersName();
-        GuessNumber guessNumber = initPlayers();
-		do {
-            initPlayers();
+        String[] playersNames = new String[2];
+        enterNames(playersNames);
+        GuessNumber guessNumber = initPlayers(playersNames);
+        do {
+            initPlayers(playersNames);
             guessNumber.gameStart();
-            repeatGame();
-		} while (answer.equals("да"));
+		} while (repeatGame().equals("да"));
 	}
 
-	private static void entersName() {
+	private static void enterNames( String[] playersNames) {
         System.out.print("Введите имя первого игрока: ");
-        playersNames[0] = scanner.next();
+        playersNames[0] = scanner.nextLine();
         System.out.print("Введите имя второго игрока: ");
-        playersNames[1] = scanner.next();
+        playersNames[1] = scanner.nextLine();
     }
 
-    private static GuessNumber initPlayers() {
+    private static GuessNumber initPlayers(String[] playersNames) {
         Player playerOne = new Player(playersNames[0]);
         Player playerTwo = new Player(playersNames[1]);
         return new GuessNumber(playerOne, playerTwo);
     }
 
     private static String repeatGame() {
+        String answer;
         do {
             System.out.print("Хотите продолжить? [да/нет]: ");
             answer = scanner.next();
