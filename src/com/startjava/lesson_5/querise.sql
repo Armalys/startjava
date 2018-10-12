b26959143ae9973f2dbbe2e4506699be0db46ca5
Armalys <mashnert@mail.ru>
2018-10-12 20:17:04 +0300

    First commit

---
+++ src/com/startjava/lesson_5/querise.sql
@@ -0,0 +1,10 @@
+SELECT * FROM Jaeger;
+SELECT * FROM Jaeger WHERE status = "Active";
+SELECT * FROM Jaeger WHERE mark = "Mark-1" and "Mark-6";
+SELECT * FROM Jaeger ORDER BY mark DESK;
+SELECT * FROM Jaeger WHERE launch = (SELECT MIN(launch) FROM Jaeger);
+SELECT * FROM Jaeger WHERE kaijuKill = (SELECT MAX(kaijuKill) FROM Jaeger);
+SELECT * FROM Jaeger WHERE kaijuKill = (SELECT MIN(kaijuKill) FROM Jaeger);
+SELECT AVG(weight) FROM Jaeger;
+UPDATE Jaeger SET kaijuKill = kaijuKill + 1 WHERE status = "Active";
+DELETE FROM Jaeger WHERE status = "Destroyed";
