db93b08e77d0048cffc9751054935d9e57f68300
Armalys <mashnert@mail.ru>
2018-10-12 21:01:36 +0300

    First commit

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -1,12 +1,12 @@
-INSER INTO Jaegers(modelName, mark, height, weight, status, origin, launch, kaijuKill)
+INSERT INTO Jaegers(modelName, mark, height, weight, status, origin, launch, kaijuKill)
 	VALUES
-		('Gipsy Danger',	'Mark-3',	79.25,    1,980,	 'Destroyed',	 'USA',	 		2017-7-10,	 	9),
-		('Gipsy Avenger',	'Mark-6',	81.77,    2.04, 	 'Destroyed', 	 'USA',  		2034-01-12, 	1),
-		('Cherno Alpha',	'Mark-1',	85.34,    2.44, 	 'Destroyed', 	 'Russia',  	2015-10-02, 	6),
-		('Striker Eureka',	'Mark-5',	76.02,    1.85, 	 'Destroyed', 	 'Australia',  	2019-08-02, 	11),
-		('Obsidian Fury',	'Mark-5',	96.02,    2.35, 	 'Destroyed', 	 'Japan',  		2029-04-15, 	3),
-		('Saber Athena',	'Mark-7',	76.91,    1.62, 	 'Destroyed', 	 'USA',  		2033-11-20, 	1),
-		('Bracer Phoenix',	'Mark-5',	70.91,    2.15, 	 'Active', 	 	 'Russia',  	2025-09-04, 	9),
-		('Titan Redeemer',	'Mark-6',	75.12,    1.45, 	 'Active', 	 	 'Australia',  	2029-07-06, 	4),
-		('Horizon Brave',	'Mark-1',	72.54,    2.05, 	 'Destroyed', 	 'China',  		2015-10-22, 	2),
-		('Scrapper',		'Mark-7',	78.41,    1.76, 	 'Active', 	 	 'USA',  		2027-11-18, 	8);
+		('Gipsy Danger',	'Mark-3',	79.25,    1.980,	 'Destroyed',	 'USA',	 		'2017-07-10',	9),
+		('Gipsy Avenger',	'Mark-6',	81.77,    2.04, 	 'Destroyed', 	 'USA',  		'2034-01-12', 	1),
+		('Cherno Alpha',	'Mark-1',	85.34,    2.44, 	 'Destroyed', 	 'Russia',  	'2015-10-02', 	6),
+		('Striker Eureka',	'Mark-5',	76.02,    1.85, 	 'Destroyed', 	 'Australia',  	'2019-08-02', 	11),
+		('Obsidian Fury',	'Mark-5',	96.02,    2.35, 	 'Destroyed', 	 'Japan',  		'2029-04-15', 	3),
+		('Saber Athena',	'Mark-7',	76.91,    1.62, 	 'Destroyed', 	 'USA',  		'2033-11-20', 	1),
+		('Bracer Phoenix',	'Mark-5',	70.91,    2.15, 	 'Active', 	 	 'Russia',  	'2025-09-04', 	9),
+		('Titan Redeemer',	'Mark-6',	75.12,    1.45, 	 'Active', 	 	 'Australia',  	'2029-07-06', 	4),
+		('Horizon Brave',	'Mark-1',	72.54,    2.05, 	 'Destroyed', 	 'China',  		'2015-10-22', 	2),
+		('Scrapper',		'Mark-7',	78.41,    1.76, 	 'Active', 	 	 'USA',  		'2027-11-18', 	8);
